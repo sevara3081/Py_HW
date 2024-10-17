35b22b9c37a50bce9a9c40935dd86f9acf0121c5
Sevara3081 <sevara3081@mail.ru>
2024-10-17 14:58:36 +0300

    Сделано пятое задание

--- 02_lesson/lesson_2_task_5.py
+++ 02_lesson/lesson_2_task_5.py
@@ -1,17 +1,28 @@
-# Функция, которая определяет сезон по номеру месяца
-def month_to_season(month):
-    if month in [12, 1, 2]:
-        return "Зима"
-    elif month in [3, 4, 5]:
-        return "Весна"
-    elif month in [6, 7, 8]:
-        return "Лето"
-    elif month in [9, 10, 11]:
-        return "Осень"
-    else:
-        return "Некорректный номер месяца"
+# pylint: disable=invalid-name
+"""
+Этот модуль содержит функцию для определения сезона по номеру месяца.
+"""
+def month_to_season(month_num):
+    """
+        Определяет сезон на основе номера месяца.
 
-# Пример использования функции
-month = 2  # Укажите любой номер месяца
+        Параметры:
+        month (int): Номер месяца (от 1 до 12).
+
+        Возвращает:
+        str: Название сезона (Зима, Весна, Лето, Осень).
+        """
+    if month_num in [12, 1, 2]:
+        return "Зима"
+    if month_num in [3, 4, 5]:
+        return "Весна"
+    if month_num in [6, 7, 8]:
+        return "Лето"
+    if month_num in [9, 10, 11]:
+        return "Осень"
+    return "Неверный номер месяца"
+
+# Пример использования:
+month = 2
 season = month_to_season(month)
 print(f"Месяц {month}: {season}")
