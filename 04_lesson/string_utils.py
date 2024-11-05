59c726777a6de55e85fd336e104d2f2ee7d1955d
Sevara3081 <sevara3081@mail.ru>
2024-11-05 12:17:00 +0300

    string_utils.py исправлено

--- 04_lesson/string_utils.py
+++ 04_lesson/string_utils.py
@@ -3,10 +3,10 @@ class StringUtils:
     Класс с полезными утилитами для обработки и анализа строк
     """
 
-    def capitilize(self, string: str) -> str:
+    def capitalize(self, string: str) -> str:
         """
         Принимает на вход текст, делает первую букву заглавной и возвращает этот же текст
-        Пример: capitilize("skypro") -> "Skypro"
+        Пример: capitalize("skypro") -> "Skypro"
         """
         return string.capitalize()
 
