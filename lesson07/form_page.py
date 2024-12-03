0a82dafe99879b6b7daaf28d16203c6843e817db
Sevara3081 <sevara3081@mail.ru>
2024-12-03 09:48:22 +0300

    form_page.py исправленный

--- lesson07/form_page.py
+++ lesson07/form_page.py
@@ -1,32 +1,33 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 
 class FormPage:
-    def __init__(self, driver):  # Исправлено название метода на __init__
-        self._driver = driver
-        self._driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
-        self._driver.implicitly_wait(15)  # Установка ожидания для всех элементов
+    def __init__(self, driver):
+        self.driver = driver
+        self.url = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
 
-    def fill_form(self, first_name, last_name, address, e_mail, phone, city, country, job_position, company):
-        """Метод для заполнения формы"""
-        self._driver.find_element(By.NAME, "first-name").send_keys(first_name)
-        self._driver.find_element(By.NAME, "last-name").send_keys(last_name)
-        self._driver.find_element(By.NAME, "address").send_keys(address)
-        self._driver.find_element(By.NAME, "e-mail").send_keys(e_mail)
-        self._driver.find_element(By.NAME, "phone").send_keys(phone)
-        self._driver.find_element(By.NAME, "city").send_keys(city)
-        self._driver.find_element(By.NAME, "country").send_keys(country)
-        self._driver.find_element(By.NAME, "job-position").send_keys(job_position)
-        self._driver.find_element(By.NAME, "company").send_keys(company)
+    def open(self):
+        self.driver.get(self.url)
 
-        # Нажимаем на кнопку "Отправить"
-        submit_button = WebDriverWait(self._driver, 30).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
+    def fill_field(self, field_name, value):
+        field = self.driver.find_element(By.CSS_SELECTOR, f"input[name='{field_name}']")
+        field.clear()
+        field.send_keys(value)
+
+    def submit_form(self):
+        WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[type="submit"]'))
+        ).click()
+
+    def wait_for_zip_code_field(self):
+        WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='zip-code']"))
         )
-        submit_button.click()
 
-        # Проверяем наличие нового элемента, чтобы удостовериться в завершении операции
-        WebDriverWait(self._driver, 30).until(
-            EC.presence_of_element_located((By.ID, "zip-code"))
-        )
+    def get_field_background_color(self, field_name):
+        field = self.driver.find_element(By.CSS_SELECTOR, f"input[name='{field_name}']")
+        return field.value_of_css_property("background-color")
